f4b1fa0433b041a3879a0dad8d8cb6703cab4f40
github username <github email address>
2017-10-31 11:43:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
-class Application < Sinatra::Base
-  # Write your code here!
+require 'pry'
 
+class Application < Sinatra::Base
+  get '/' do
+    "Welcome!"
+    erb :index
+  end
+
+  post '/greet' do
+
+    erb :greet
+  end
 end
